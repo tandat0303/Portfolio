73e83e6586894e452c731c604a9c62f59bbefe0c
Dat <ls17189a3.11@gmail.com>
2025-08-17 22:24:45 +0700

    update info

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
+import food_chatbot from "../../Assets/Projects/food_chatbot.png";
+import FaceDetection from "../../Assets/Projects/FaceDetection.png";
 import PDUL from "../../Assets/Projects/posDecLED.png";
 import dem_system from "../../Assets/Projects/dem_system.png";
 import suicide from "../../Assets/Projects/suicide.png";
@@ -54,29 +54,29 @@ function Projects() {
             />
           </Col>
 
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={food_chatbot}
+              isBlog={false}
+              title="Food Chatbot - Search Recipes App"
+              description="This project develops an AI-powered cooking chatbot that assists users in recipe suggestions based on available ingredients, personalized meal planning, and step-by-step cooking guidance."
+              ghLink="https://github.com/tandat0303/Food_Chabot"
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={FaceDetection}
+              isBlog={false}
+              title="Student attendance"
+              description="..."
+              ghLink="https://github.com/tandat0303/StudentAttendance"
+              demoLink=""
+            />
+          </Col>
+
           {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
